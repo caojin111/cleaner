613d36c27aecd73b42174b1124a16be31360c6ba
Caojin <596730256@qq.com>
2025-07-10 11:48:28 +0800

    Initial Commit

---
+++ CleanUpAi/CleanUpAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanUpAiApp.swift
+//  CleanUpAi
+//
+//  Created by Allen on 2025/7/10.
+//
+
+import SwiftUI
+
+@main
+struct CleanUpAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanUpAiTests/CleanUpAiTests.swift
@@ -0,0 +1,16 @@
+//
+//  CleanUpAiTests.swift
+//  CleanUpAiTests
+//
+//  Created by Allen on 2025/7/10.
+//
+
+import Testing
+
+struct CleanUpAiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
